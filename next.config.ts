eb4f88141ea7b367aa32d55361a143fa083ada90
Aditya <adityaguptavarsheny@gmail.com>
2025-05-06 21:25:30 +0530

    fixing deployment 4 .. i guess

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,13 @@
+import path from "path";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     eslint: {
         ignoreDuringBuilds: true,
+    },
+    webpack: (config) => {
+        config.resolve.alias["@"] = path.resolve(__dirname);
+        return config;
     },
 };
 
